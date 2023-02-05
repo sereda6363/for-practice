f6fde59a5dca6ce7856fb91ee305cf8b814e22d1
Stelmashchuk Sergii <sereda636322@gmail.com>
2023-02-06 00:19:46 +0200

    sdfsf

--- js/pricol.js
+++ js/pricol.js
@@ -1,10 +1,19 @@
 "use strict";
 
-const string = "Halllo world!!! А это уже втрой документ";
-const consoleIf = string.length
-const text = 'это длинна строки'
+// const string = "Halllo world!!! А это уже втрой документ";
+// const consoleIf = string.length
+// const text = 'это длинна строки'
 
-console.log(`${string} ${consoleIf} ${text}`)
+// console.log(`${string} ${consoleIf} ${text}`)
 
-let number = Math.ceil(Math.random() * (100 - 1) +1)
-alert(number)
+// let number = Math.ceil(Math.random() * (100 - 1) +1)
+// alert(number)
+
+// пройдёмся по тернарным операторам
+
+const age = 19;
+
+if (age > 18)
+  console.log('Вы старше 18 лет');
+else
+  console.log('Вы младше 18 лет')
